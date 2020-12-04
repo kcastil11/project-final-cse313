5aec877ff27b397ba5b198baf7ddd622be59684f
Mayabel <user@Retina.local>
2020-12-03 23:42:03 -0800

    first

---
+++ config.js
@@ -0,0 +1,7 @@
+const constants = {
+    openWeatherMap:{
+        BASE_URL: "http://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY:   "6b047ecf2489a06435b658599bc1d407"
+    }
+}
+module.exports = constants;
